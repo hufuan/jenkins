f74e04ba097c963d95fb772f1b51dfe7aa3865f4
hufuan <fuan_707@163.com>
2022-02-11 16:42:11 +0800

    test

---
+++ junit5example/src/main/java/com/fuhu/junit5example/Junit5exampleApplication.java
@@ -0,0 +1,13 @@
+package com.fuhu.junit5example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5exampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5exampleApplication.class, args);
+    }
+
+}
---
+++ junit5example/src/test/java/com/fuhu/junit5example/Junit5exampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuhu.junit5example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5exampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
